8614d0da9e21ba4a6084a638ca8e5e74bba34e5f
Nishchal Bafna <68919560+Nishchal-Bafna@users.noreply.github.com>
2020-07-31 20:53:06 +0530

    Update Count Triplets such that one of the numbers can be written as sum of the other two.py

--- Count Triplets such that one of the numbers can be written as sum of the other two.py
+++ Count Triplets such that one of the numbers can be written as sum of the other two.py
@@ -17,3 +17,18 @@ def check():
 for i in range(0,n):
 	a[i]=int(input('enter {} element  '.format(i+1)))
 check()
+
+
+
+
+'''
+output
+enter range of an array = 6
+enter 1 element  1
+enter 2 element  2
+enter 3 element  3
+enter 4 element  4
+enter 5 element  5
+enter 6 element  6
+( 1+1=2 ) ; ( 1+2=3 ) ; ( 1+3=4 ) ; ( 1+4=5 ) ; ( 1+5=6 ) ; ( 2+2=4 ) ; ( 2+3=5 ) ; ( 2+4=6 ) ; ( 3+3=6 ) ; so the count is =  9
+'''
